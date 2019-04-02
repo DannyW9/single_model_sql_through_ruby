0ce512e725186b03aede8ec26e919d7e03454a13
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-02 18:55:22 +0100

    adds method for updating instances stored within the database

--- console.rb
+++ console.rb
@@ -8,3 +8,14 @@ bounty1 = Bounty.new({'name' => 'Han Solo',
                     })
 
 bounty1.save()
+
+bounty2 = Bounty.new({'name' => 'Chewbacca',
+                      'species' => 'Wookie',
+                      'bounty_value' => '50000',
+                      'cashed_in' => 'false'
+                      })
+
+bounty2.save()
+
+bounty1.cashed_in = "true"
+bounty1.update()
--- models/bounty.rb
+++ models/bounty.rb
@@ -2,11 +2,15 @@ require('pg')
 
 class Bounty
 
+  attr_accessor :name, :species, :bounty_value, :cashed_in
+  attr_reader :id
+
   def initialize(details)
     @name = details['name']
     @species = details['species']
     @bounty_value = details['bounty_value'].to_i
     @cashed_in = details['cashed_in']
+    @id = details['id'].to_i if details['id']
   end
 
   def save()
@@ -19,15 +23,30 @@ class Bounty
       cashed_in
     )
     VALUES ($1, $2, $3, $4)
+    RETURNING id
     "
     values = [@name, @species, @bounty_value, @cashed_in]
     db.prepare("save", sql)
-    db.exec_prepared("save", values)
+    @id = db.exec_prepared("save", values)[0]['id'].to_i
     db.close()
   end
 
   def update()
+    db = PG.connect( {dbname: 'bounty_hunter', host: 'localhost'})
 
+    sql = "UPDATE bounties
+    SET (
+      name,
+      species,
+      bounty_value,
+      cashed_in
+    ) =
+    ( $1, $2, $3, $4 )
+    WHERE id = $5"
+    values = [@name, @species, @bounty_value, @cashed_in, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close()
   end
 
 
